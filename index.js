543a98ce7c243f20d42cf1299e7f46b08e072ffc
ar_parvez <arparvez50@gmail.com>
2023-12-17 18:15:34 +0600

    Second commit - designer user created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Developer = require("./Developer");
+const Designer = require('./Designer')
 const Elephant = require("./Elephant");
 const Rocky = new Developer("Rocky Jack", "rocky.contact@gmail.com", [
   "Java",
@@ -14,6 +15,18 @@ let php = new Elephant("PHP", 29, "Indigo");
 let laravel = new Elephant("PHP", 29, "Indigo");
 
 // console.log(JSON.stringify(php) == JSON.stringify(laravel));
-console.log(php.color);
-php.color = 'Yellow'
-console.log(php.color);
+// console.log(php.color);
+// php.color = 'Yellow'
+// console.log(php.color);
+
+let Zahid = new Designer('Zahid Hasan','zahid@example.com',['Figma','XD','Sketch'])
+// console.log(Zahid.toString());
+
+
+console.log(Zahid.prof.join(', '));
+
+Zahid.prof = ['Photoshop','Illustrator','InDesign']
+console.log(Zahid.prof.join(', '));
+
+
+
